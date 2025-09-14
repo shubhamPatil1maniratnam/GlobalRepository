636debc2f769974db582b149b762e2bcb6dc5cbe
srj21091998-wq <srj21091998@gmail.com>
2025-09-15 00:13:56 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,30 @@
-class company
-    companyname = 'xyz'
-def__init__(self,id,name,city)
-    self.id = id
-    self.name = name
-    self.city = city
-def show(self)
-print(companyname)
+class Company:
+    companyname = "IBM"
+    def __init__(self,id,name,city):
+        self.id = id
+        self.name = name
+        self.city = city
+
+    def show(self):
+        print("Company Name:",Company.companyname)
+        print("ID:",self.id)
+        print("Name:",self.name)
+        print("City:",self.city)
+
+    @staticmethod
+    def add(x,y):
+        return x+y
+
+    @classmethod
+    def companyname_change(cls):
+        cls.companyname = "Infosys"
+
+obj = Company(101,'Rahul','Pune')
+obj.show()
+Company.companyname_change()
+
+
+obj.show()
+
+result = obj.add(100,300)
+print(result)
