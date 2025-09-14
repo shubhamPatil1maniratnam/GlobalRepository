6665e0d949527be02599f312a014309d91611a01
srj21091998-wq <srj21091998@gmail.com>
2025-09-15 00:49:23 +0530

    Update primary_node1.py

--- primary_node1.py
+++ primary_node1.py
@@ -1,6 +1,31 @@
-class student :
-    def __init__ (self,name,age,gender,bg):
+class Company:
+    companyname = "IBM"
+    def __init__(self,id,name,city):
+        self.id = id
         self.name = name
-        self.age = age
-        self.gender = gender
-        self.bg = bloodgrouGIT A
+        self.city = city
+
+    def show(self):
+        print("Company Name:",Company.companyname)
+        print("ID:",self.id)
+        print("Name:",self.name)
+        print("City:",self.city)
+
+    @staticmethod
+    def add(x,y):
+        return x+y
+
+    @classmethod
+    def companyname_change(cls):
+        cls.companyname = "Infosys"
+
+
+obj = Company(101,'Rahul','Pune')
+obj.show()
+Company.companyname_change()
+
+
+obj.show()
+
+result = obj.add(100,300)
+print(result)
